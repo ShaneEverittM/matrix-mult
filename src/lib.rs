76ab44b04baef2cfb4b43d42ae78f78e0b3248e9
ShaneEverittM <shaneeverittmurphy@gmail.com>
2020-12-18 23:32:37 -0800

    Indexing and drop.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::fmt::{Debug, Formatter, self};
+use std::ops::{Index, IndexMut};
+use std::mem;
 
 pub struct Matrix {
-    ptr: *const i32,
+    ptr: *mut i32,
     side_length: usize,
 }
 
@@ -26,12 +28,16 @@ impl Debug for Matrix {
 
 
 impl Matrix {
-    pub fn new(m: &[&[i32]]) -> Self {
+    pub fn new(m: Vec<Vec<i32>>) -> Self {
         assert!(m.iter().all(|e| e.len() == m.len()), "Matrix must be square");
-        let x = unsafe { (*m.as_ptr()).as_ptr() };
+        let len = m.len();
+        let flat_vec = m.into_iter().flatten().collect::<Vec<i32>>();
+        let ptr = flat_vec.as_ptr() as *mut _;
+        // This pointer own the vec now.
+        mem::forget(flat_vec);
         Self {
-            ptr: x,
-            side_length: m.len(),
+            ptr,
+            side_length: len,
         }
     }
 
@@ -43,21 +49,50 @@ impl Matrix {
     }
 }
 
+impl Index<usize> for Matrix {
+    type Output = [i32];
+
+    fn index(&self, index: usize) -> &Self::Output {
+        unsafe { std::slice::from_raw_parts(self.ptr.offset((index * self.side_length) as isize), self.side_length) }
+    }
+}
+
+impl IndexMut<usize> for Matrix {
+    fn index_mut(&mut self, index: usize) -> &mut [i32] {
+        unsafe { std::slice::from_raw_parts_mut((self.ptr as *mut i32).offset((index * self.side_length) as isize), self.side_length) }
+    }
+}
+
+impl Drop for Matrix {
+    fn drop(&mut self) {
+        let total_length = self.side_length * self.side_length;
+        let upper_bound_power_2 = |mut n: usize| {
+            let mut count = 0;
+            if n != 0 && (n & (n - 1)) == 0 {
+                n
+            } else {
+                while n != 0 {
+                    n >>= 1;
+                    count += 1;
+                }
+                1 << count
+            }
+        };
+        let cap = upper_bound_power_2(total_length);
+        let _ = unsafe { std::vec::Vec::from_raw_parts(self.ptr, total_length, cap) };
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use crate::Matrix;
 
     #[test]
-    fn vec_as_ptr() {
-        let row1: [i32; 2] = [1, 2];
-        let row2: [i32; 2] = [3, 4];
-        let mat: [&[i32]; 2] = [&row1, &row2];
-        let mut m = Matrix::new(&mat);
-        unsafe {
-            assert_eq!(*m.at(1, 1), 4);
-            dbg!(&m);
-            assert_eq!(*m.at(1, 1), 4);
-        }
+    fn indexing() {
+        let v = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let mut m = Matrix::new(v);
+        assert_eq!(2, m[0][1]);
+        m[1][1] = 1;
+        dbg!(m);
     }
 }
